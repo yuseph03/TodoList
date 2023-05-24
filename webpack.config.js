fa006450fdb4dc9ea3af967db174351c790b2e39
Yuseph <vernonnashville@gmail.com>
2023-05-24 11:12:37 +0330

    Add Asset packages

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,25 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
   },
+  module: {
+    rules: [
+     {
+       test: /\.(png|svg|jpg|jpeg|gif)$/i,
+       type: 'asset/resource',
+     },
+     {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+     },
+     {
+      test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      type: 'asset/resource',
+    }
+    ],
+  },  
 };
