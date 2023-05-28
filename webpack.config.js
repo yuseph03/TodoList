a20a294327c7978e1e43ebe062f0ae576640475a
Yuseph <vernonnashville@gmail.com>
2023-05-28 08:47:00 +0330

    Add HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'TodoList',
+    }),
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
